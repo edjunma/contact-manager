80ada12a70bf785e427e45cff940d61dff748ad2
edjunma <junwenma@buffalo.edu>
2019-03-21 22:50:31 -0400

    Created a form with state, controlled component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Contacts from "./components/contacts/Contacts";
 import Header from "./components/layout/Header";
-import AddContact from "./components/contact/AddContact";
+import AddContact from "./components/contacts/AddContact";
 import { Provider } from "./context";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
