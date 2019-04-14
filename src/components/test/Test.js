231f517764967d867665faf97ebeb310d9b8dc11
edjunma <junwenma@buffalo.edu>
2019-04-14 01:54:53 -0400

    added get http request to test

--- src/components/test/Test.js
+++ src/components/test/Test.js
@@ -1,34 +1,51 @@
 import React from "react";
 
 class Test extends Component() {
+  state = {
+    test: "test"
+  };
+
   componentDidMount() {
-    console.log("componentDidMount...");
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          title: data.title,
+          body: data.body
+        })
+      );
   }
 
-  componentWillMount() {
-    console.log("componentWillMount...");
-  }
+  // componentWillMount() {
+  //   console.log("componentWillMount...");
+  // }
 
-  componentDidUpdate() {
-    console.log("componentDidUpdate...");
-  }
+  // componentDidUpdate() {
+  //   console.log("componentDidUpdate...");
+  // }
 
-  componentWillUpdate() {
-    console.log("componentWillUpdate...");
-  }
+  // componentWillUpdate() {
+  //   console.log("componentWillUpdate...");
+  // }
 
-  componentWillReceiveProps(nextProps, nextState) {
-    console.log("componentWillReceiveProps...");
-  }
+  // componentWillReceiveProps(nextProps, nextState) {
+  //   console.log("componentWillReceiveProps...");
+  // }
 
   static getDerivedStateFromProps(nextProps, prevState) {
     return null;
   }
 
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("getSnapshotBeforeUpdate...");
+  }
+
   render() {
+    const { title, body } = this.state;
     return (
       <div>
-        <h1>Test Component</h1>
+        <h1>{title}</h1>
+        <p>{body}</p>
       </div>
     );
   }
