a4f055bc2e06e4965be42ea89c4aaad7586a5b42
edjunma <junwenma@buffalo.edu>
2019-05-14 02:23:44 -0400

    Refactor await/async in Contact/AddContact

--- src/components/contacts/Contact.js
+++ src/components/contacts/Contact.js
@@ -4,58 +4,61 @@ import { Consumer } from "../../context";
 import axios from "axios";
 
 class Contact extends Component {
-  state = {
-    showContactInfo: false
-  };
+	state = {
+		showContactInfo: false
+	};
 
-  onDeleteClick = (id, dispatch) => {
-    axios
-      .delete(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then(res => dispatch({ type: "DELETE_CONTACT", payload: id }));
-  };
+	onDeleteClick = async (id, dispatch) => {
+		try {
+			await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+			dispatch({ type: "DELETE_CONTACT", payload: id });
+		} catch (e) {
+			dispatch({ type: "DELETE_CONTACT", payload: id });
+		}
+	};
 
-  render() {
-    const { id, name, email, phone } = this.props.contact;
-    const { showContactInfo } = this.state;
-    return (
-      <Consumer>
-        {value => {
-          const { dispatch } = value;
-          return (
-            <div className="card card-body mb-3">
-              <h4>
-                {name}{" "}
-                <i
-                  onClick={() =>
-                    this.setState({
-                      showContactInfo: !this.state.showContactInfo
-                    })
-                  }
-                  className="fas fa-sort-down"
-                  style={{ cursor: "pointer" }}
-                />
-                <i
-                  className="fas fa-times"
-                  style={{ cursor: "pointer", float: "right", color: "red" }}
-                  onClick={this.onDeleteClick.bind(this, id, dispatch)}
-                />
-              </h4>
-              {showContactInfo ? (
-                <ul className="list-group">
-                  <li className="list-group-item">Email: {email}</li>
-                  <li className="list-group-item">Phone: {phone}</li>
-                </ul>
-              ) : null}
-            </div>
-          );
-        }}
-      </Consumer>
-    );
-  }
+	render() {
+		const { id, name, email, phone } = this.props.contact;
+		const { showContactInfo } = this.state;
+		return (
+			<Consumer>
+				{value => {
+					const { dispatch } = value;
+					return (
+						<div className='card card-body mb-3'>
+							<h4>
+								{name}{" "}
+								<i
+									onClick={() =>
+										this.setState({
+											showContactInfo: !this.state.showContactInfo
+										})
+									}
+									className='fas fa-sort-down'
+									style={{ cursor: "pointer" }}
+								/>
+								<i
+									className='fas fa-times'
+									style={{ cursor: "pointer", float: "right", color: "red" }}
+									onClick={this.onDeleteClick.bind(this, id, dispatch)}
+								/>
+							</h4>
+							{showContactInfo ? (
+								<ul className='list-group'>
+									<li className='list-group-item'>Email: {email}</li>
+									<li className='list-group-item'>Phone: {phone}</li>
+								</ul>
+							) : null}
+						</div>
+					);
+				}}
+			</Consumer>
+		);
+	}
 }
 
 Contact.propTypes = {
-  contact: PropTypes.object.isRequired
+	contact: PropTypes.object.isRequired
 };
 
 export default Contact;
