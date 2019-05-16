0f0c5d623bf0e6fe10a0cfe0d1479becae702c95
edjunma <junwenma@buffalo.edu>
2019-05-16 01:50:32 -0400

    finished CRUD, need to review and debug

--- src/components/contacts/EditContact.js
+++ src/components/contacts/EditContact.js
@@ -43,7 +43,19 @@ class EditContact extends Component {
 		if (phone === "") {
 			this.setState({ errors: { phone: "Phone is required" } });
 			return;
-		}
+    }
+
+    const updContact = {
+      name,
+      email,
+      phone
+    }
+    
+    const { id } = this.props.match.params;
+
+    const res = await axios.put(`https://jsonplaceholder.typicode.com/users/${id}`, updContact);
+
+    dispatch({type:'UPDATE_CONTACT', payload: res.data});
 
 		// Clear state
 		this.setState({
